09cbe75c42d9225849d7d97faa4df1f630154c75
Rodrigo Salinas <ras123gg@gmail.com>
2022-10-05 20:12:40 -0300

    fix db.ts

--- db.ts
+++ db.ts
@@ -1,5 +1,5 @@
 import * as admin from "firebase-admin";
-import * as serviceAccount from "./key.json";
+var serviceAccount = require("./key.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as any),
